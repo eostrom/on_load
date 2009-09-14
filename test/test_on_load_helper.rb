a52132c290c870f5da3722f7192a2408bf51d889
Erik Ostrom <erik@echographia.com>
2009-09-13 23:24:44 -0700

    Test refactoring.

--- test/test_on_load_helper.rb
+++ test/test_on_load_helper.rb
@@ -12,40 +12,38 @@ class TestOnLoadHelper < ActionController::TestCase
   end
   
   test 'on_load treats a string as JavaScript' do
-    assert_equal(
-      %{document.observe('dom:loaded', function() {true && false})\n},
+    assert_on_load(
+      %[document.observe('dom:loaded', function() {true && false})],
       on_load('true && false'))
   end
   
   test 'on_load puts newlines around a multiline string' do
-    assert_equal([
-        "document.observe('dom:loaded', function() {",
-        "true && false;\nfalse && true;",
-        "})\n"
-      ].join("\n"),
+    assert_on_load(
+      %[
+        document.observe('dom:loaded', function() {
+          true && false;
+          false && true;
+        })
+      ],
       on_load("true && false;\nfalse && true;"))
   end
   
   test "on_load treats a no-args block in ERB as JavaScript" do
     get :on_load_with_block_with_no_args
 
-    assert_match(
-      code_regexp(%[
+    assert_on_load(
+      %[
         document.observe('dom:loaded', function() {
           alert('block');
         })
-      ]),
-      @response.body)
+      ])
   end
 
   test "on_load treats a one-arg block like update_page" do
     get :on_load_with_block_with_one_arg
     
-    assert_match(
-      code_regexp(%[
-        document.observe('dom:loaded', function() {alert("hi!");})\n
-      ]),
-      @response.body)
+    assert_on_load(
+      %[document.observe('dom:loaded', function() {alert("hi!");})])
   end
 
 protected
@@ -56,5 +54,9 @@ protected
         Regexp.quote(line.strip)
       end.join('\s+'),
       'm')
+  end
+  
+  def assert_on_load(expected, actual = @response.body)
+    assert_match(code_regexp(expected), actual.strip)
   end
 end
